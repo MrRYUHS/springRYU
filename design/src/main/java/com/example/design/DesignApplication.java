63f4e1b2960454bd7f1e4c612515be48e8ca30ff
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-05-10 21:34:37 +0900

    어뎁터패턴

--- design/src/main/java/com/example/design/DesignApplication.java
+++ design/src/main/java/com/example/design/DesignApplication.java
@@ -1,5 +1,6 @@
 package com.example.design;
 
+import com.example.design.adapter.*;
 import com.example.design.singleton.AClazz;
 import com.example.design.singleton.BClazz;
 import com.example.design.singleton.SocketClient;
@@ -8,6 +9,7 @@ public class DesignApplication {
 
     public static void main(String[] args){
 
+        /*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -16,6 +18,21 @@ public class DesignApplication {
 
         System.out.println("???");
         System.out.println(aClient.equals(bClient));
+        */
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+    }
+
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 
 }
