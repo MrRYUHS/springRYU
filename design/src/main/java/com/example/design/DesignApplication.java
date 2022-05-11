e687712f8a8b772a3d11f63ae8473fd5bbb63d0b
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-05-11 21:12:54 +0900

    프록시패턴

--- design/src/main/java/com/example/design/DesignApplication.java
+++ design/src/main/java/com/example/design/DesignApplication.java
@@ -1,34 +1,52 @@
 package com.example.design;
 
 import com.example.design.adapter.*;
+import com.example.design.aop.AopBrowser;
+import com.example.design.proxy.Browser;
+import com.example.design.proxy.BrowserProxy;
+import com.example.design.proxy.IBrowser;
 import com.example.design.singleton.AClazz;
 import com.example.design.singleton.BClazz;
 import com.example.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class DesignApplication {
 
     public static void main(String[] args){
-
         /*
-        AClazz aClazz = new AClazz();
-        BClazz bClazz = new BClazz();
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
 
-        SocketClient aClient = aClazz.getSocketClient();
-        SocketClient bClient = bClazz.getSocketClient();
-
-        System.out.println("???");
-        System.out.println(aClient.equals(bClient));
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
         */
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
 
-        Cleaner cleaner = new Cleaner();
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
     }
 
     public static void connect(Electronic110V electronic110V){
---
+++ design/src/main/java/com/example/design/proxy/Html.java
@@ -0,0 +1,9 @@
+package com.example.design.proxy;
+
+public class Html {
+    private String url;
+
+    public Html(String url){
+        this.url = url;
+    }
+}
