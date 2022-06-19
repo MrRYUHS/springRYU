ffff0fa1ed7c39b7c4a56111a4a2cf4abaee6236
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-06-20 00:54:49 +0900

    DI

---
+++ ioc/src/main/java/Encoder.java
@@ -0,0 +1,14 @@
+import java.util.Base64;
+
+public class Encoder {
+
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder){
+        this.iEncoder = iEncoder;
+    }
+    public String encode(String message){
+
+        return Base64.getEncoder().encodeToString(message.getBytes());
+    }
+}
--- ioc/src/main/java/Main.java
+++ ioc/src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("main");
+        String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
+
+        Encoder encoder= new Encoder(new Base64Encoder());
+        String result = encoder.encode(url);
+        System.out.println(result);
     }
 }
