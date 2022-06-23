7d4bf755de05be498019d1f555bc74236b434a67
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-06-23 23:46:07 +0900

    aop

---
+++ spring-aop/src/main/java/com/example/springaop/aop/ParameterAop.java
@@ -0,0 +1,12 @@
+package com.example.springaop.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect // aop로 동작하기 위해 적음
+@Component // 스프링에서 관리되어야 해서 적음
+public class ParameterAop {
+    @Pointcut("execution(* com.example.springaop.controller..*.*(..))")
+    private void cut(){}
+}
--- spring-aop/src/main/java/com/example/springaop/controller/RestApiController.java
+++ spring-aop/src/main/java/com/example/springaop/controller/RestApiController.java
@@ -1,15 +1,19 @@
 package com.example.springaop.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springaop.dto.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
-    public void get(){
-
+    @GetMapping("/get/{id}")
+    public void get(@PathVariable Long id, @RequestParam String name){
+        System.out.println("get method");
+        System.out.println("get method : "+id);
+        System.out.println("get method : "+name);
     }
-    public void post(){
-
+    @PostMapping("/post")
+    public void post(@RequestBody User user){
+        System.out.println("post method : "+user);
     }
 }
