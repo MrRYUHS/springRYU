886eaa8de4bb9d2bba7c77928d8e457ba2d23d9e
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-07-17 02:40:33 +0900

    Rest Template

--- client/src/main/java/com/example/client/dto/Req.java
+++ client/src/main/java/com/example/client/dto/Req.java
@@ -1,2 +1,50 @@
-package com.example.client.dto;public class Req {
+package com.example.client.dto;
+
+public class Req<T> {
+    private Header header;
+
+    private T respBody;
+
+    public static class Header{
+        private String responseCode;
+
+        public String getResponseCode() {
+            return responseCode;
+        }
+
+        public void setResponseCode(String responseCode) {
+            this.responseCode = responseCode;
+        }
+
+        @Override
+        public String toString() {
+            return "Header{" +
+                    "responseCode='" + responseCode + '\'' +
+                    '}';
+        }
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public T getRespBody() {
+        return respBody;
+    }
+
+    public void setRespBody(T respBody) {
+        this.respBody = respBody;
+    }
+
+    @Override
+    public String toString() {
+        return "Req{" +
+                "header=" + header +
+                ", body=" + respBody +
+                '}';
+    }
 }
--- server/src/main/java/com/example/server/dto/Req.java
+++ server/src/main/java/com/example/server/dto/Req.java
@@ -1,2 +1,20 @@
-package com.example.server.dto;public class Req {
+package com.example.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Req<T> {
+    private Header Header;
+    private T respBody;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Header{
+        private String responseCode;
+    }
 }
