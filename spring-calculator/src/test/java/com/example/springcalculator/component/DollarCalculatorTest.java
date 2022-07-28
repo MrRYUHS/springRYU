6cd2b4906a925abcb8ede544aa360f8a4dda6c6c
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2022-07-28 21:33:01 +0900

    연습

--- spring-calculator/src/test/java/com/example/springcalculator/component/DollarCalculatorTest.java
+++ spring-calculator/src/test/java/com/example/springcalculator/component/DollarCalculatorTest.java
@@ -6,7 +6,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 public class DollarCalculatorTest {
