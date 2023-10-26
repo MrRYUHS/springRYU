18a96819ce3ef46b4662e743cbe7fdf5974a9c7d
HyeongSoo <rhsrsy123@gmail.com>
2023-10-26 15:55:03 +0900

    멋쟁이사자처럼 7일차

---
+++ lion01/src/main/java/org/example/Quotation.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Quotation {
+    int id;
+    String content;
+    String authorName;
+
+    public Quotation(int id, String content, String authorName){
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
