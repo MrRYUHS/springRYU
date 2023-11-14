f9a2ca579f018e077f0a005890400c6a782afeb7
HyeongSoo <rhsrsy123@gmail.com>
2023-11-14 10:47:42 +0900

    231114실습생성

---
+++ lion231114/src/main/java/com/example/lion231114/Lion231114Application.java
@@ -0,0 +1,13 @@
+package com.example.lion231114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lion231114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lion231114Application.class, args);
+    }
+
+}
---
+++ lion231114/src/test/java/com/example/lion231114/Lion231114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lion231114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lion231114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
